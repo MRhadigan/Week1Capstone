8da585cc0e43890c91a2b8932ebe679e24c39c1e
Matthew <mrhadigan@gmail.com>
2018-07-22 22:15:19 -0400

    second commit

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -16,23 +16,27 @@ public class PigLatin {
 		String userOut = "";
 		String userOutTwo = "";
 		
+		//Explaining the game
 		System.out.println("Translating from English to Pig Latin");
 		
+		// Do while loop to ask if they want to continue, if they do it will loop back to the top
 		do {
 		System.out.println("Please enter a word!");
 		userWord = scan.nextLine();
 		int i = 0;
 		
 		
-		
+		// counting through the word and processing each letter entered
 		for (i = 0; i < userWord.length(); i++) {
 			
+			// if first letter is a vowel it will print the word + way
 			if (vowels.contains("" + userWord.charAt(0))){
 				userOutTwo = userWord + "way";
 				System.out.println(userOutTwo);
 				break;
 			}
 			
+			// otherwise it will find the vowel and move the letters before it to the end, and add "ay" at the end
 			if (vowels.contains("" + userWord.charAt(i))) {
 				firstLetters = userWord.substring(0, i);
 				userWord = userWord.substring(i);
@@ -47,7 +51,7 @@ public class PigLatin {
 			}
 		
 		
-			
+		// Finishes the do while loop and asks to continue
 		
 		System.out.println("Would you like to enter another word? (y/n)");
 		cont = scan.nextLine();
